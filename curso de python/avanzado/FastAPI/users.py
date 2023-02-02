49fb8adc88b2a2d08fe827733eb650c66a58e8f8
Jheral Barrera <jheral.barrera@gmail.com>
2023-02-02 15:10:08 -0300

    Sistema CRUD

--- curso de python/avanzado/FastAPI/users.py
+++ curso de python/avanzado/FastAPI/users.py
@@ -53,6 +53,44 @@ async def user(id: int):
 async def user(id: int):
     return search_user(id)
 
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"error": "El usuario ya existe"}
+    else:
+        users_list.append(user)
+        return user
+
+@app.put("/user/")
+async def user(user: User):
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+
+    if not found:
+        return {"error": "No se ha actualizado el usuario"}
+
+    return user
+
+@app.delete("/user/{id}")
+async def user(id: int):
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+        
+    if not found:
+        return {"error": "Usuario no eliminado"}
+
+
+
 # Funcion que realiza el filtro por "id"
 def search_user(id: int):
     users = filter(lambda user: user.id == id, users_list)
